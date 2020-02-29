2cf7c815a4daf69077bef6aed9c1634f2179646c
Alontay Ellis <VinceExito@Alontays-MacBook-Air-2.local>
2020-02-29 12:03:21 -0800

    User Input Python Practice Scripts

---
+++ DevOpsPracticeScripts.py
@@ -0,0 +1,13 @@
+input1="input"
+>>> print(input1)
+input
+>>> input1="hola"
+>>> print(input1.upper())
+
+input2="Mississippi"
+len(input2)
+
+input3="Biggest"
+suffix="est"
+print(input3.rstrip(suffix))
+
